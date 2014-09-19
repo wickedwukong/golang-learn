6a6706e00aed168d2aabb49938b221d288cef20c
Xuemin Guan <wickedwukong@Xuemins-MacBook-Air.local>
2014-09-19 11:26:59 +0100

    learning about embeded type

--- src/EmbededStructLearn.go
+++ src/EmbededStructLearn.go
@@ -10,9 +10,26 @@ func (person *Person) Talk() {
 	fmt.Println("Hi, my name is", person.Name)
 }
 
+type Android struct {
+	Person Person
+	Model string
+}
+
+type Droid struct {
+	Person
+	Model string
+}
+
 func main() {
 	person := Person{Name: "Xuemin"}
 
 	person.Talk()
+
+	seeTHREEPIO := Android{Person: Person{Name: "SEE-THREEPIO"}, Model: "C-3PO"}
+
+	seeTHREEPIO.Person.Talk()
+
+	r2D2 := Droid{Person: Person{Name: "R2-D2"}, Model: "R2-D2"}
+	r2D2.Talk()
 }
 
