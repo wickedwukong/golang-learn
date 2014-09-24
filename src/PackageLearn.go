9bed87c507cee7c2978bfb23e20a45caccab40e8
Xuemin Guan <gxuemin@gmail.com>
2014-09-24 23:34:45 +0100

    learning about package

---
+++ src/PackageLearn.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "xuemin"
+
+func main() {
+	fmt.Println("name: ", xuemin.Name())
+	fmt.Println("age: ", xuemin.Age())
+}
---
+++ src/xuemin/test.go
@@ -0,0 +1,9 @@
+package xuemin
+
+func Name() string {
+	return "Xuemin"
+}
+
+func Age() int {
+	return 100
+}
